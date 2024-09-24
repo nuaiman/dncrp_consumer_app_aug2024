d20e991aef5b1ede7255d7e34ccbd0d6473efe1a
Nuaiman Ashiq <76113072+nuaiman@users.noreply.github.com>
2024-09-24 14:28:23 +0600

    Delete lib directory

--- lib/apis/baseurl.dart
+++
@@ -1,5 +0,0 @@
-const String baseUrl = 'http://192.168.0.113:7041';
-
-const String fileUploadUrl = '$baseUrl/fl/upld';
-const String fileViewUrl =
-    '$baseUrl/images/evidences-akajsdfljasdfoewruower93427';
--- lib/core/constants/pngs.dart
+++
@@ -1,4 +0,0 @@
-class Pngs {
-  static const assetPath = 'assets/pngs';
-  static const logo = '$assetPath/logo.png';
-}
